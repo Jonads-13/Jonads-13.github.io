9f2ba7a92e866ed22d82b50fb5d8416588757cf8
Jake Jonas <example@curtin.com>
2025-02-24 05:52:22 +0800

    added tooltips and reference to og portfolio

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import ContactPage from "@mui/icons-material/ContactPage";
+import Tooltip from "@mui/material/Tooltip";
 import '../assets/styles/Footer.scss'
 
 function Footer() {
   return (
     <footer>
       <div>
-        <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-        <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+      <Tooltip title='GitHub' placement="top">
+              <a href="https://github.com/Jonads-13" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            </Tooltip>
+
+            <Tooltip title="LinkedIn" placement="top">
+              <a href="https://www.linkedin.com/in/jacob-jonas-892095a8/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            </Tooltip>
+
+            <Tooltip title="Resume" placement="top">
+              <a href="https://drive.google.com/file/d/12ft-zX34m9PWXGawcdRJ5WJHwdb1zy6n/view?usp=sharing" target="_blank" rel="noreferrer"><ContactPage/></a>
+            </Tooltip>
       </div>
-      <p>A portfolio designed & built by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Yuji Sato</a> with 💜</p>
+      <p>A portfolio adapted from <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">react-portfolio-template</a></p>
     </footer>
   );
 }
